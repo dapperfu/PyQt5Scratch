b2a31d0dd4209d028e2177825e9ce5ad749abccd
Jed <jed-frey@users.noreply.github.com>
2016-12-11 04:52:51 -0500

    Git Auto Commit: Sun Dec 11 04:52:51 EST 2016

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 control_gui.py: control_gui.ui
 	@echo "Building $? -> $@"
-#	pyuic5 $(OBJS) -o $(SRCS)
-
+	pyuic5 $? -o $@
+	@echo ""
---
+++ control_gui.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'control_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 20))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuAbout = QtWidgets.QMenu(self.menubar)
+        self.menuAbout.setObjectName("menuAbout")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionQuit = QtWidgets.QAction(MainWindow)
+        self.actionQuit.setObjectName("actionQuit")
+        self.actionAbout = QtWidgets.QAction(MainWindow)
+        self.actionAbout.setObjectName("actionAbout")
+        self.actionEaster_Eggs = QtWidgets.QAction(MainWindow)
+        self.actionEaster_Eggs.setObjectName("actionEaster_Eggs")
+        self.menuFile.addAction(self.actionQuit)
+        self.menuAbout.addAction(self.actionAbout)
+        self.menuAbout.addAction(self.actionEaster_Eggs)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuAbout.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuAbout.setTitle(_translate("MainWindow", "Help"))
+        self.actionQuit.setText(_translate("MainWindow", "Quit"))
+        self.actionAbout.setText(_translate("MainWindow", "About"))
+        self.actionEaster_Eggs.setText(_translate("MainWindow", "Easter Eggs"))
+
