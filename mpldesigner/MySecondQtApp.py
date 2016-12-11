b347788b9771f13854b52fd679e24de21665710d
Jed <jed-frey@users.noreply.github.com>
2016-12-11 04:32:51 -0500

    Git Auto Commit: Sun Dec 11 04:32:51 EST 2016

--- mpldesigner/MySecondQtApp.py
+++ mpldesigner/MySecondQtApp.py
@@ -1,9 +1,50 @@
+
+# coding: utf-8
+
+# In[1]:
+
 from PyQt5.uic import loadUiType
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
+
+
+# In[2]:
+
+from control.matlab import *
+
+
+# In[3]:
+
+s=tf([1,0],[1])
+
+
+# In[4]:
+
+a=3
+b=2
+c=1
+Plant = a/(b*s+c)
+
+t=0
+t_final = 100
+dt = 0.01
+T = np.arange(t, t_final+dt, dt)
+
+
+# In[5]:
+
+X0 = 0.0
+
+
+# In[6]:
+
+yout, T = step(Plant, T=T, X0=X0)
+
+
+# In[2]:
 
 Ui_MainWindow, QMainWindow = loadUiType('window.ui')
         
@@ -17,13 +58,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         fig = Figure()
         self.addmpl(fig)
-        
-    def do_nothing(self, item):
-        for key, value in item.items():
-            if isinstance(value, str):
-                print("{}: {}".format(key, value))
-        print("")                
-            
 
     def changefig(self, item):
         text = item.text()
@@ -44,26 +78,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.mplvl.addWidget(self.toolbar)
 # This is the alternate toolbar placement. Susbstitute the three lines above
 # for these lines to see the different look.
-#        self.toolbar = NavigationToolbar(self.canvas, self, coordinates=True)
+#        self.toolbar = NavigationToolbar(self.canvas,
+#                self, coordinates=True)
 #        self.addToolBar(self.toolbar)
-    def rmmpl(self, *args, **kw_args):
-        for arg in args:
-            print(arg)
-        for key, value in kw_args.items():
-            print("{}: {}".format(key, value))
+
+    def rmmpl(self,):
         self.mplvl.removeWidget(self.canvas)
         self.canvas.close()
         self.mplvl.removeWidget(self.toolbar)
         self.toolbar.close()
 
+
+# In[ ]:
+
 if __name__ == '__main__':
     import sys
     from PyQt5 import QtWidgets
-    import numpy as np
-
+    from pylab import *
+    from control.matlab import *
+    
+    yout, T = step(Plant, T=T, X0=X0)
     fig1 = Figure()
     ax1f1 = fig1.add_subplot(111)
-    ax1f1.plot(np.random.rand(5))
+    ax1f1.plot(T,yout)
 
     fig2 = Figure()
     ax1f2 = fig2.add_subplot(121)
@@ -82,3 +119,4 @@ if __name__ == '__main__':
     main.addfig('Pcolormesh', fig3)
     main.show()
     sys.exit(app.exec_())
+
