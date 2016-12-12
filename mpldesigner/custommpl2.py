747a9ec047524e5534acd997a5c9e1373388bcc1
Jed <jed-frey@users.noreply.github.com>
2016-12-12 15:07:57 -0500

    Git Auto Commit: Mon Dec 12 15:07:57 EST 2016

--- mpldesigner/custommpl2.py
+++ mpldesigner/custommpl2.py
@@ -4,6 +4,8 @@ from PyQt5.uic import loadUiType
 from PyQt5 import QtWidgets
 import sys
 
+import numpy as np
+
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
@@ -12,11 +14,26 @@ from matplotlib.backends.backend_qt5agg import (
 Ui_MainWindow, QMainWindow = loadUiType('window2.ui')
 
 class ControlCanvas(FigureCanvas):
-    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
-
+    """ Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.).
+    """
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
         self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+        
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def update():
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
         
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, ):
